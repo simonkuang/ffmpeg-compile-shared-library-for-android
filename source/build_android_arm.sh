55264dfb3836b0cf802f1b0f1ca01ae6889d1d2c
simonkuang <me@simonkuang.com>
2017-01-18 17:56:26 +0800

    change the variables

--- source/build_android_arm.sh
+++ source/build_android_arm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-export TMPDIR=/home/djia/tmpdir
-NDK=/home/djia/android/android-ndk-r10
-SYSROOT=$NDK/platforms/android-16/arch-arm/
-TOOLCHAIN=/home/djia/android/android-ndk-r10/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64
+export TMPDIR=/Volumes/RamDisk/abcd
+NDK=/Users/simonkuang/Library/Android/sdk/ndk-bundle
+SYSROOT=$NDK/platforms/android-16/arch-arm/android-24/arch-arm
+TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64
 
 CPU=arm
-PREFIX=/root/workspace/ffmpeg_shared_compile/dxjia_ffmpeg_install/arm/
+PREFIX=/Users/simonkuang/workspace/ffmpeg_shared_compiled/arm/
 ADDI_CFLAGS="-marm"
 
 function build_one
